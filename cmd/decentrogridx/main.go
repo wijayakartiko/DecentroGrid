bfd9ace400b95905298dd1b7a95d7e5290c2e659
Silvian Negri <wijayakartiko9926@carox.tech>
2026-07-16 22:43:31 +0700

    Add main.go
    Co-authored-by: nancodero <nancodero@proton.me>

---
+++ cmd/decentrogridx/main.go
@@ -0,0 +1,23 @@
+// cmd/decentrogridx/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "decentrogridx/internal/decentrogridx"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := decentrogridx.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
